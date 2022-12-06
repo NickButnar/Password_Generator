f5e5c60f57c7d5872e7ef65e4cc67c0780f767db
NickButnar <103021759+NickButnar@users.noreply.github.com>
2022-12-06 21:04:26 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,67 @@
-const generatedPassword = function donePassword(passwordLength) {
-  const numCHars = "0123456789";
-  const digraphsChars =
-    "eeeaaiauayoioyooowoaoualkeerturetionssionchckbtghwrworwar";
+function donePassword(passwordLength) {
+  const consonantArray = [];
+  consonantArray.push(
+    "ble",
+    "ple",
+    "dle",
+    "tle",
+    "gle",
+    "fle",
+    "cle",
+    "ckle",
+    "stle",
+    "zle"
+  );
 
-  const allChars = numCHars + digraphsChars;
-  const noNumberChars = digraphsChars;
+  const vowelArray = [];
+  vowelArray.push(
+    "ai",
+    "au",
+    "ay",
+    "aw",
+    "ea",
+    "ee",
+    "ei",
+    "ie",
+    "oa",
+    "oo",
+    "ou",
+    "ow",
+    "are",
+    "ar",
+    "ere",
+    "ire",
+    "ure"
+  );
 
-  let numbers = true;
+  const numberChars = "0,1,2,3,4,5,6,7,8,9";
+  const numberArray = numberChars.split(",");
+  const letterChars =
+    "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+  const letterArray = letterChars.split(",");
+
   let password = "";
 
-  if (numbers) {
-    for (let i = 0; i < passwordLength; i++) {
-      let randomNumber = Math.floor(Math.random() * allChars.length);
+  const consonantChars = letterArray + numberArray + consonantArray;
+  const vowelChars = letterArray + numberArray + vowelArray;
 
-      password += allChars[randomNumber];
+  // Чередование согласных и гласных сочетаний
+
+  for (let i = 0; i < passwordLength; i++) {
+    if (i % 2 === 0) {
+      let randomNumber = Math.floor(Math.random() * consonantChars.length);
+
+      password += consonantChars[randomNumber];
     }
 
-    return password;
-  }
+    if (i % 2 == 0) {
+      let randomNumber = Math.floor(Math.random() * vowelChars.length);
 
-  if (numbers === false) {
-    for (let i = 0; i < passwordLength; i++) {
-      let randomNumber = Math.floor(Math.random() * noNumberChars.length);
-
-      password += noNumberChars[randomNumber];
+      password += vowelChars[randomNumber];
     }
-
-    return password;
   }
-};
+  return password;
+}
+console.log(donePassword(12));
 
-console.log(generatedPassword(12));
-
-export default { generatedPassword };
+export default { donePassword };
