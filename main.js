d9ee3e8b9e02dbc26345f6da339907c1f89a360f
NickButnar <103021759+NickButnar@users.noreply.github.com>
2022-12-15 21:14:53 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,67 +1,33 @@
-function donePassword(passwordLength) {
-  const consonantArray = [];
-  consonantArray.push(
-    "ble",
-    "ple",
-    "dle",
-    "tle",
-    "gle",
-    "fle",
-    "cle",
-    "ckle",
-    "stle",
-    "zle"
-  );
+const donePassword = (passwordLength, number) => {
+  const letterChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numberChars = "0123456789";
 
-  const vowelArray = [];
-  vowelArray.push(
-    "ai",
-    "au",
-    "ay",
-    "aw",
-    "ea",
-    "ee",
-    "ei",
-    "ie",
-    "oa",
-    "oo",
-    "ou",
-    "ow",
-    "are",
-    "ar",
-    "ere",
-    "ire",
-    "ure"
-  );
+  let resultDonePassword = "";
+  let resultLetterPassword = "";
+  let resultNumbersPassword = "";
 
-  const numberChars = "0,1,2,3,4,5,6,7,8,9";
-  const numberArray = numberChars.split(",");
-  const letterChars =
-    "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
-  const letterArray = letterChars.split(",");
-
-  let password = "";
-
-  const consonantChars = letterArray + numberArray + consonantArray;
-  const vowelChars = letterArray + numberArray + vowelArray;
-
-  // Чередование согласных и гласных сочетаний
-
-  for (let i = 0; i < passwordLength; i++) {
-    if (i % 2 === 0) {
-      let randomNumber = Math.floor(Math.random() * consonantChars.length);
-
-      password += consonantChars[randomNumber];
-    }
-
-    if (i % 2 == 0) {
-      let randomNumber = Math.floor(Math.random() * vowelChars.length);
-
-      password += vowelChars[randomNumber];
+  if (number > 0 && number <= passwordLength) {
+    for (let i = 0; i < number; i++) {
+      let randomNumberOfNumbers = Math.floor(
+        Math.random() * numberChars.length
+      );
+      resultNumbersPassword += numberChars[randomNumberOfNumbers];
     }
   }
-  return password;
-}
-console.log(donePassword(12));
+
+  if (number === 0) {
+    for (let i = 0; i < passwordLength; i++) {
+      let randomNumberOfLetters = Math.floor(
+        Math.random() * letterChars.length
+      );
+      resultLetterPassword += letterChars[randomNumberOfLetters];
+    }
+  }
+
+  resultDonePassword = resultNumbersPassword + resultLetterPassword;
+  return resultDonePassword;
+};
+
+console.log(donePassword(6, 0));
 
 export default { donePassword };
